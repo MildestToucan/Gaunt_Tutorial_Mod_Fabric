28668511daa7b4429ff7a6a3cec5fcdf40bb93fe
MildToucan <introvertpale@gmail.com>
2025-09-27 05:53:58 +0200

    Better fit tutorial

--- src/main/java/net/gauntrecluse/gaunt_tutorial_mod/mixin/TutorialFirstMixin.java
+++ src/main/java/net/gauntrecluse/gaunt_tutorial_mod/mixin/TutorialFirstMixin.java
@@ -1,7 +1,12 @@
 package net.gauntrecluse.gaunt_tutorial_mod.mixin;
 
 import net.gauntrecluse.gaunt_tutorial_mod.GauntTutorialMod;
+import net.minecraft.network.QueryableServer;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.ServerTask;
+import net.minecraft.server.command.CommandOutput;
+import net.minecraft.server.world.ChunkErrorHandler;
+import net.minecraft.util.thread.ReentrantThreadExecutor;
 import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,11 +22,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
  */
 @Debug(export = true)
 @Mixin(MinecraftServer.class)
-public abstract class TutorialFirstMixin {
-	@Inject(method = "loadWorld",
+public abstract class TutorialFirstMixin extends ReentrantThreadExecutor<ServerTask> implements QueryableServer, ChunkErrorHandler, CommandOutput, AutoCloseable {
+    //This is a dummy constructor, implemented just to extend the target's parent class.
+    //It'll be ignored by Mixin while merging.
+    public TutorialFirstMixin(String string) {
+        super(string);
+    }
+
+    @Inject(method = "loadWorld",
             at = @At(value = "HEAD")
     )
-	private void addLoggerAtHead(CallbackInfo info) {
+	private void addLoggerAtHead(CallbackInfo ci) {
         GauntTutorialMod.LOGGER.info("MinecraftServer$loadWorld has started!");
 	}
 }
